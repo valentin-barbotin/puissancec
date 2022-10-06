9095ff19a5ae8d1509ab95dd1da9999eb0ed719e
flammefolie_kraken <fanny.gt10@gmail.com>
2022-10-06 18:32:54 +0200

    print1_grille_vide

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/fanny/Documents/puissancec
+# Build directory: C:/Users/fanny/Documents/puissancec/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- main.c
+++ main.c
@@ -32,4 +32,38 @@ int main(int argc, char **argv) {
     {
         puts(users->users[i].name);
     }
+    int rows = 6;
+    int columns = 7;
+
+    char grille[rows][columns];
+
+    for (int i = 0; i < rows ; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            grille[i][j]='X';
+        }
+    }
+
+    //affichage grille
+    for (int i = 0; i < rows ; ++i) {
+        for (int j = 0; j <  columns; ++j) {
+            //printf("%c", grille[i][j]);
+            if(j==columns-1){
+                //printf("\n");
+            }
+        }
+    }
+
+    //affichage complet
+    for (int i = 0; i < rows ; ++i) {
+        printf("|");
+        for (int j = 0; j <  columns; ++j) {
+            printf("  %c  |",grille[i][j]);
+            if(j==columns-1){
+                printf("\n");
+            }
+        }
+    }
+
+    return 0;
+
 }
