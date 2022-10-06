cac4564f76a2721d3369b36b086da1afa0045496
flammefolie_kraken <fanny.gt10@gmail.com>
2022-10-06 18:32:54 +0200

    print1_grille_vide

--- main.c
+++ main.c
@@ -2,5 +2,38 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    puts("Hello world");
+    int rows = 6;
+    int columns = 7;
+
+    char grille[rows][columns];
+
+    for (int i = 0; i < rows ; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            grille[i][j]='X';
+        }
+    }
+
+    //affichage grille
+    for (int i = 0; i < rows ; ++i) {
+        for (int j = 0; j <  columns; ++j) {
+            //printf("%c", grille[i][j]);
+            if(j==columns-1){
+                //printf("\n");
+            }
+        }
+    }
+
+    //affichage complet
+    for (int i = 0; i < rows ; ++i) {
+        printf("|");
+        for (int j = 0; j <  columns; ++j) {
+            printf("  %c  |",grille[i][j]);
+            if(j==columns-1){
+                printf("\n");
+            }
+        }
+    }
+
+    return 0;
+
 }
