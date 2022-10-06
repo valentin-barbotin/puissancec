53e98041fb35efb0073f29325834fba070e4a5f3
flammefolie_kraken <fanny.gt10@gmail.com>
2022-10-06 22:53:05 +0200

    preparation_rebase

--- printGrille.h
+++ printGrille.h
@@ -5,4 +5,8 @@
 #ifndef PUISSANCEC_PRINTGRILLE_H
 #define PUISSANCEC_PRINTGRILLE_H
 
+char ** init_grille(int rows, int columns);
+void print_grille(char ** grille, int rows, int columns);
+void print_all(char ** grille, int rows, int columns);
+
 #endif //PUISSANCEC_PRINTGRILLE_H
